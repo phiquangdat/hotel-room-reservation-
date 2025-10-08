2fdbf2f55cb79748bbfce82a2187e6980d11aa1a
phiquangdat <datphi020603@gmail.com>
2025-10-08 20:15:13 +0300

    Update documentation structure and remove outdated files

---
+++ backend/src/test/java/com/team_seven/hotel_reservation_system/HotelReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team_seven.hotel_reservation_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
