dd3a6cec054a3f0960104479d2a57eab11377c7b
phiquangdat <datphi020603@gmail.com>
2025-10-16 21:14:37 +0300

    Create Spring Data JPA Repositories for User and Role

---
+++ backend/src/main/java/com/team_seven/hotel_reservation_system/models/Role.java
@@ -0,0 +1,20 @@
+package com.team_seven.hotel_reservation_system.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 50, unique = true, nullable = false)
+    private String name;
+}
---
+++ backend/src/main/java/com/team_seven/hotel_reservation_system/models/User.java
@@ -0,0 +1,20 @@
+package com.team_seven.hotel_reservation_system.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+    private String password;
+}
