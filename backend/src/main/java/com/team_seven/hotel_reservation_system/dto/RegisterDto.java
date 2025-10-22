aa86b59759518f17acf2a3ee3453a983ec9949f1
dongxubin87 <dongxubin87@gmail.com>
2025-10-22 23:36:35 +0300

    endpoin

---
+++ backend/src/main/java/com/team_seven/hotel_reservation_system/dto/RegisterDto.java
@@ -0,0 +1,14 @@
+package com.team_seven.hotel_reservation_system.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterDto {
+    private String username;
+    private String email;
+    private String password;
+}
