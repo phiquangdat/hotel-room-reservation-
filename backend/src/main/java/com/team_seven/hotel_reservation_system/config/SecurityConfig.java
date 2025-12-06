4a2d13cb33347db54b24ce5a0cf7b7d17a98d89c
phiquangdat <datphi020603@gmail.com>
2025-12-06 23:01:03 +0200

    Add backend security to frontend

--- backend/src/main/java/com/team_seven/hotel_reservation_system/config/SecurityConfig.java
+++ backend/src/main/java/com/team_seven/hotel_reservation_system/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://127.0.0.1:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://127.0.0.1:3000", "https://hotel-room-reservation-tan.vercel.app" ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
